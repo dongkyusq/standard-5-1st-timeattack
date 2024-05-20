40a22c7b130e5339294ab4e95a108813be924df0
dongkyusq <hasd789@naver.com>
2024-05-20 15:52:58 +0900

    미완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 export default function App() {
+  const [counter, setCounter] = React.useState(0);
+  const plusClick = () => {
+    setCounter(counter + 1);
+  };
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
+        <input /> 만큼을 <button onClick={plusClick}>더할게요</button>{" "}
+        <button>뺄게요</button>
         <button>초기화</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{counter}</p>
       </div>
     </div>
   );
